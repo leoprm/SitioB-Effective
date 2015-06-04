6d3467018202196e277b3178adcc43ff27048792
Leonardo Rivas <leoprm@gmail.com>
2015-06-04 15:33:34 -0300

    Primeros pasos

---
+++ Home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Beffective</title>
+	<style type="text/css">
+		body{
+			background: red; 
+		}
+	</style>
+</head>
+<body>
+
+
+<ol>
+	<li>consultoria
+	<li>atencion
+	<li>prestaciones
+	<li>equipos
+	<li>sonido
+</ol>
+
+</body>
+</html>
