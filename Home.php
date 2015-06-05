ad3516ed8ebcf016e321e13bfd6572886d6491e9
Leonardo Rivas <leoprm@gmail.com>
2015-06-05 00:58:32 -0300

    Maqueta del sitio

--- Home.php
+++ Home.php
@@ -6,18 +6,20 @@
 		body{
 			background: red; 
 		}
+
 	</style>
 </head>
 <body>
 
 
-<ol>
+<ol>Opciones
 	<li>consultoria
 	<li>atencion
 	<li>prestaciones
 	<li>equipos
 	<li>sonido
 </ol>
+
 
 </body>
 </html>
